c0cb05519b1bed13bbe0183d88fa8e28e3bf8744
Ollie Embley <ollieembley@MacBook-Air.local>
2023-02-22 12:35:20 +0000

    6 tests

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
 const express = require("express");
 const app = express();
 const {getCategories} = require("./controllers/categoriesControllers")
-const {handle500Statuses, handle404Statuses } = require("./controllers/errorHandlingControllers")
+const {handle500Statuses, handle404Statuses, handleCustomErrors, handle400Errors } = require("./controllers/errorHandlingControllers")
 const {getReviews} = require("./controllers/reviewsControllers")
+const {getReviewID} = require("./controllers/reviewsControllers")
+const {getComments} = require("./controllers/reviewsControllers")
+
 
 
 
 app.get("/api/categories", getCategories);
 app.get("/api/reviews", getReviews);
+app.get("/api/reviews/:review_id", getReviewID);
+app.get("/api/reviews/:review_id/comments", getComments);
+
 
 
 app.use((request, response, next) => {
     response.status(404).send({msg: 'path not found!'})
 })
 
-
 app.use(handle404Statuses);
-
 app.use(handle500Statuses);
+app.use(handle400Errors);
+app.use(handleCustomErrors);
+
 
 
 
--- controllers/reviewsControllers.js
+++ controllers/reviewsControllers.js
@@ -1,4 +1,8 @@
 const {selectReviews} = require("../models/reviewsModels");
+const {selectReview} = require("../models/reviewsModels");
+const {selectComments} = require("../models/reviewsModels");
+
+
 
 exports.getReviews = (request, response, next) => {
     selectReviews().then((reviews) => {
@@ -10,8 +14,25 @@ exports.getReviews = (request, response, next) => {
     })
     };
 
+exports.getReviewID = (request, response, next) => {
+    selectReview(request.params.review_id)
+    .then((review) => {
+        response.status(200).send({review});
+    })
+    .catch((err) => 
+    next (err));
+};
 
-    
+exports.getComments = (request, response, next) => {
+    const {review_id} = request.params;
+    selectComments(review_id)
+    .then((res) => {
+        response.status(200).send({comments :res});
+    })
+    .catch((error) => 
+    next(error));
+};
+
 
 
 
